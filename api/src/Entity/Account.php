ca94ef5eb2cf416dbfe5d7694bdad3761fff5227
Near <near@Nears-MacBook-Pro.local>
2022-09-01 13:10:16 +0430

    data generator added

--- api/src/Entity/Account.php
+++ api/src/Entity/Account.php
@@ -8,7 +8,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity(repositoryClass: AcountRepository::class)]
+#[ORM\Entity(repositoryClass: AccountRepository::class)]
 #[ApiResource]
 class Account
 {
